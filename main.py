7528e34303fcce17f1d8fbacdbdeca597400d47a
Твоё Имя <ivan.ivanov@example.com>
2025-10-17 08:55:27 +0300

    добавили secondmain.py и изменили main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, World!")
+print("Hello, World! X2")
 input("Press Enter to exit...")  # Чтобы консоль не закрывалась
 print("This is an update from the second commit!")
 input("Press Enter to exit...")
