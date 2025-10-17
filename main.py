0b3ff2554280669d7fcaaddb0ddadbf49f332e25
Твоё Имя <ivan.ivanov@example.com>
2025-10-17 08:55:57 +0300

    Убрали комментарии

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("Hello, World! X2")
-input("Press Enter to exit...")  # Чтобы консоль не закрывалась
+input("Press Enter to exit...")
 print("This is an update from the second commit!")
 input("Press Enter to exit...")
---
+++ secondmain.py
@@ -0,0 +1,2 @@
+print("Hello, World! X3")
+input("Press Enter to exit")
